af85bc970cfc1514abc7108d91d6c34a354807fb
JorshTab <jorshm.tabpar@educa.jcyl.es>
2026-02-05 19:40:29 +0100

    Commit change sysout print now Hola Mundo2

---
+++ miProyectoEJ1/src/miProyectoEJ1/Main.java
@@ -0,0 +1,8 @@
+package miProyectoEJ1;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo2");
+	}
+
+}
---
+++ miProyectoEJ1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module miProyectoEJ1 {
+}
